cde573eb52e6478a8b86e5341c7a403c9d6daf2f
Андрей <info@bustep.ru>
2023-11-17 12:59:45 +0700

    Update TestCase.php

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -15,8 +15,8 @@ abstract class TestCase extends MockeryTestCase
 {
     protected function setUp(): void
     {
-		define('FC_REST_URL', "https://rest.massive.ru/");
-		define('FC_REST_TOKEN', "VXDTB9lg4Uz4vkKsASAx2");
+		define('FC_REST_URL', "");
+		define('FC_REST_TOKEN', "");
 		require_once dirname(__DIR__, 1) . '/vendor/autoload.php';
         parent::setUp();
     }
